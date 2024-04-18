f3b4fbd3e92cbe17d84166cc3f683403281a8178
Marek Sierociński <marek.sierocinski@cdq.com>
2024-04-18 19:10:32 +0200

    feat: Add release event

--- src/update-majorver.ts
+++ src/update-majorver.ts
@@ -1,19 +1,20 @@
 import * as core from "@actions/core";
 import { getOctokit, context } from "@actions/github";
 
-const REFS_TAG = "refs/tags/";
+const TAGS = "tags/";
+const REFS_TAGS = "refs/" + TAGS;
 
 export default async function run(): Promise<void> {
   try {
     const token = core.getInput("github_token");
     const octokit = getOctokit(token);
 
-    if (!context.ref.startsWith(REFS_TAG)) {
+    if (!context.ref.startsWith(REFS_TAGS)) {
       core.setFailed("ref is not a tag");
       return;
     }
 
-    const tag = context.ref.substring(REFS_TAG.length);
+    const tag = context.ref.substring(REFS_TAGS.length);
     const regex = /^v?(?<major>\d+)\.\d+\.\d+$/;
     const match = tag.match(regex);
     if (match?.groups?.major == null) {
@@ -24,35 +25,44 @@ export default async function run(): Promise<void> {
     }
 
     const major = `v${match.groups.major}`;
-    const sha = context.payload.head_commit.id;
 
     const refParams = {
       owner: context.repo.owner,
       repo: context.repo.repo,
-      ref: REFS_TAG + major,
     };
 
     let ref;
     try {
-      ref = await octokit.git.getRef(refParams);
+      ref = await octokit.git.getRef({
+        ...refParams,
+        ref: TAGS + major,
+      });
       core.info(`tag ${major} already exists`);
     } catch (error) {
+      if (core.isDebug()) {
+        core.debug(error);
+      }
       core.info(`tag ${major} does not exist yet`);
     }
 
     if (ref) {
       await octokit.git.updateRef({
         ...refParams,
-        sha,
+        ref: TAGS + major,
+        sha: context.sha,
         force: true,
       });
     } else {
       await octokit.git.createRef({
         ...refParams,
-        sha,
+        ref: REFS_TAGS + major,
+        sha: context.sha,
       });
     }
   } catch (error) {
+    if (core.isDebug()) {
+      core.debug(error);
+    }
     core.setFailed(error.message);
   }
   return;
